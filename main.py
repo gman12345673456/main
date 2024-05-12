43d947778199e73e07c4fc6bd59106498f2e1e20
Gus <gsimmonds@student.saintaug.nsw.edu.au>
2024-05-13 09:45:07 +1000

    lbl text works

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import gooeypie as gp
 
 app = gp.GooeyPieApp("Hello!")
 
-app.width = 1000
+app.width = 600
+app.height = 400
+app.title = "Password Checker"
 
+app.set_grid(2,1)
+
+def submit(event):
+    lbl.text = "I work"
+
+btn = gp.Button(app, "Submit", submit)
+lbl = gp.Label(app, "This is a label")
+
+app.add(btn, 1,1)
+app.add(lbl,2,1)
 app.run()
